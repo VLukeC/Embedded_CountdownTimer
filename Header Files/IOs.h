20d1b3ed75e474e5e152ae56ff4fa49b4d60c4cc
Luke Chayer <luke@smellow.com>
2025-10-21 23:56:03 -0600

    Nearly Fully working IO - Header

--- Header Files/IOs.h
+++ Header Files/IOs.h
@@ -3,16 +3,15 @@
 
 #include <xc.h>
 extern volatile uint8_t longPress;     
-extern volatile uint8_t pressDuration;
 extern volatile uint8_t pressActive;
-extern volatile uint8_t pressDone;
 extern uint8_t min, sec;
 extern uint8_t paused;
-extern uint8_t ioFlag;
 extern uint8_t pb1Event, pb2Event, pb3Event;
 
 // Function declarations
 void IOinit();
+
+void increment_delay(void);
 
 void IOcheck();
 
