827262a70d8034aa91cba50b4bab145cdd9a0ff8
Landon Reed <144170201+LReed03@users.noreply.github.com>
2025-10-25 11:11:08 -0600

    Added comments to everything

--- Source Files/IOs.c
+++ Source Files/IOs.c
@@ -9,43 +9,48 @@
 #include "UART2.h"
 #include "countdown.h"
 
+// Variables
+extern uint8_t pb1, pb2, pb3;   // Button states
+extern uint8_t min=0, sec=0;    // Timer minutes and seconds
+extern uint8_t T1flag;          // Timer 1 flag
+extern uint8_t T2flag;          // Timer 2 flag
+extern uint8_t running;         // Flag for if the program is running
+extern uint8_t paused;          // Flag for if the program is paused
+extern uint8_t pb1Event, pb2Event, pb3Event;    // Flag for if a button press has occured
+extern volatile uint8_t longPress;          // Flag for if the button is held for long period
 
-extern uint8_t pb1, pb2, pb3;
-extern uint8_t min=0, sec=0;
-extern uint8_t T1flag;
-extern uint8_t T2flag;
-extern uint8_t running;
-extern uint8_t paused;
-extern uint8_t pb1Event, pb2Event, pb3Event;
-extern volatile uint8_t longPress;
+// Initilizes all the Led, pbs and CN interrupts
 void IOinit() {
     // LEDS
-    TRISBbits.TRISB9 = 0;
-    LATBbits.LATB9 = 0;
+    TRISBbits.TRISB9 = 0;   //RB9 output
+    LATBbits.LATB9 = 0;     // LED starts off
 
-    TRISAbits.TRISA6 = 0;
-    LATAbits.LATA6 = 0;
+    TRISAbits.TRISA6 = 0;   //RA6 output
+    LATAbits.LATA6 = 0;     //LED starts off
     
     // CN Interrupt config
     
-    IPC4bits.CNIP = 6;     
-    IFS1bits.CNIF = 0;
+    IPC4bits.CNIP = 6;     // CN interrupt priority level
+    IFS1bits.CNIF = 0;     // Clear CN flag
     
     // PB setup
     
-    TRISBbits.TRISB7 = 1;
-    CNPU2bits.CN23PUE = 1;
-    CNEN2bits.CN23IE = 1;  
+    // RB7 setup    
+    TRISBbits.TRISB7 = 1;      // RB7 as input
+    CNPU2bits.CN23PUE = 1;      //Enable pullup
+    CNEN2bits.CN23IE = 1;       // Enable CN interrupt
     
-            
-    TRISBbits.TRISB4 = 1;
-    CNPU1bits.CN1PUE = 1;
-    CNEN1bits.CN1IE = 1;
+    // RB4 setup
+    TRISBbits.TRISB4 = 1;   // RB4 as input
+    CNPU1bits.CN1PUE = 1;   // Enable pullup
+    CNEN1bits.CN1IE = 1;    // Enable CN interrupt
     
-    TRISAbits.TRISA4 = 1;
-    CNPU1bits.CN0PUE = 1;
-    CNEN1bits.CN0IE = 1;
+    // RA4 setup
+    TRISAbits.TRISA4 = 1;   // RA4 as input
+    CNPU1bits.CN0PUE = 1;   // Enable pullup
+    CNEN1bits.CN0IE = 1;    // Enable CN interrupt
     
+    // Enable system wide CN interrupts
     IEC1bits.CNIE = 1;
     
     
@@ -53,26 +58,32 @@ void IOinit() {
 
 //Delays incrementing of value while button held.
 void increment_delay(void){
-    T1flag = 0;
-    TMR1 = 0;
-    T1CONbits.TON = 1;
+    T1flag = 0;     // Clear flag
+    TMR1 = 0;       // Reset timer 1 counter
+    T1CONbits.TON = 1;  // Turn timer 1 back on
 }
 
+
+// Checks which button is pressed and handles the 
 void IOcheck(void) {
     // Only one press type is handled at a time for simplicity
+    // When the timer is not running
     if (!running) {
         //PB 1 Press
         if (pb1Event && !pb2Event && !pb3Event) {
             while(pressActive && !pb2Event){// PB1 short or long hold
+                // When there is a longhold
                 if (longPress && T1flag ==1) {
                     increment_delay();
                     sec += 5;
+                //Short press
                 } else if(T1flag == 1){
                     increment_delay();
                     sec += 1;
                 }
+                // If sec becomes 60 make sec = 0
                 if (sec > 59) sec = 0;
-                DispTime(min, sec);
+                DispTime(min, sec); // Update display
             }
         }
         // PB 2 pressed
@@ -80,28 +91,33 @@ void IOcheck(void) {
             // PB2 short press
             while(pressActive && !pb1Event){
                 if(T1flag == 1){
+                    // Add 1 minute per interval
                     increment_delay();
                     min+=1;
                 }
-                if (min > 59) min = 0;
-                DispTime(min, sec);
+                if (min > 59) min = 0; //Wrap around at 60 minutes
+                DispTime(min, sec); // Display time
             }
             
         }
         //PB 3 pressed
         else if(pb3Event && !pb1Event && !pb2Event){
             if(paused){
+                // Short press -> start timer
                 running = 1;
-                T2CONbits.TON = 1; 
+                T2CONbits.TON = 1;  // Resume Timer 2
             }
         }
         // PB1 and PB2 pressed
         else if (pb1Event && pb2Event && !pb3Event) {
             while(pressActive && !longPress){} //Wait to see if long press is triggered while buttons held
+            // If no long press while holding buttons
             if (!longPress) {
                 running = 1;
                 startTimer(min, sec);
-            } else {
+            } 
+            // IF there is a long press rest timer values
+            else {
                 min = 0;
                 sec = 0;
                 DispTime(min, sec);
@@ -111,12 +127,14 @@ void IOcheck(void) {
         else if (pb2Event && pb3Event && !pb1Event) {
             while(pressActive){
                 if(pb2Event){
+                    // Clears pb2 flag
                     pb2Event = 0;
+                    // Decrements minuts
                     min--;
                     if(min == 255){
                         min = 59;
                     }
-                    DispTime(min, sec);
+                    DispTime(min, sec); // Displays updated time
                 }
             }
         }
@@ -124,18 +142,21 @@ void IOcheck(void) {
         else if (pb1Event && pb3Event && !pb2Event) {
             while(pressActive){
                 if(pb1Event){
+                    // Clears pb1 flag
                     pb1Event = 0;
+                    // Decrements seconds
                     sec--;
                     if(sec == 255){
                         sec = 59;
                     }
-                    DispTime(min, sec);
+                    DispTime(min, sec); // Displats updated time
                 }
             }
         }
         // All Three held
         else if(pb1Event && pb2Event && pb3Event){
             while(pressActive){
+                // Display group info
                 Disp2String("\r2025 ENSF 460 L02 - Group 02");
             }
             //Clear line and display timer again
@@ -146,14 +167,18 @@ void IOcheck(void) {
         // clear all events after handling
         pb1Event = pb2Event = pb3Event = 0;
     }
+    // When the timer is running
     if(running){
+        // PB3
         if(pb3Event && !pb1Event && !pb2Event){
             while(pressActive && !longPress){} // Wait to see if long press while held
             if (!longPress){
+                // If there is a short press, just pause the countdown
                 T2CONbits.TON = 0; 
                 running = 0;
                 paused = 1;
             }
+            // If there is a longpause reset the timer and make sure it is not running
             else {
                 running = 0;
                 min = 0;
@@ -161,7 +186,7 @@ void IOcheck(void) {
                 DispTime(min, sec);
             }
         }
-        pb1Event = pb2Event = pb3Event = 0;
+        pb1Event = pb2Event = pb3Event = 0; // Reset the pb event flags
     }
     
 }
--- Source Files/countdown.c
+++ Source Files/countdown.c
@@ -10,55 +10,71 @@
 #include "countdown.h"
 #include "UART2.h"
 #include "IOs.h"
-extern uint8_t alarm;
-volatile uint8_t countdown = 0;
-extern uint8_t T2flag;
-extern uint8_t running;
-extern uint8_t T1flag;
+extern uint8_t alarm;       // Flag indicating that the alarm is active
+volatile uint8_t countdown = 0; // Total remaining countdown time in seconds
+extern uint8_t T2flag;      // Set by Timer 2 ISR every 1 second
+extern uint8_t running; // Indicates if the countdown is running
+extern uint8_t T1flag;  // Flag for T1Interrupt
 
+/**
+Starts the timer
+@param min Minutes to count down from
+@param sec Seconds to count down from
+*/
 void startTimer(uint8_t min, uint8_t sec){
+    // Display initial time
     DispTime(min,sec);
+    // Set running flag to 1 to start the timer
     running = 1;
+    // count down is equal to total time in seconds
     countdown = min*60 + sec;
     LATBbits.LATB9 = !LATBbits.LATB9; // switch for LED
 }
 
+
+// Updates the timer as the countdown continues
 void updateTimer(void) {
+    // Start timer 2 if it is not already running
     if (!T2CONbits.TON) {
-        TMR2 = 0;
-        IFS0bits.T2IF = 0;
-        T2flag = 0;
-        T2CONbits.TON = 1;
+        TMR2 = 0;   // Reset timer 2 counter
+        IFS0bits.T2IF = 0;  //Clear interrupt flag
+        T2flag = 0;     // Clear t2flag
+        T2CONbits.TON = 1;  // Enable timer 2
     }
 
+    // T2 flag is set every 1 second from T2 interrupt
     if (T2flag) {
-        T2flag = 0;
+        T2flag = 0;     //Reset flag
 
-        LATBbits.LATB9 ^= 1; 
+        LATBbits.LATB9 ^= 1; //Toggle Led every second
 
+        // If the countdown is greater then 0 decrement
         if (countdown > 0) {
             countdown--;
-            min = countdown / 60;
-            sec = countdown % 60;
+            min = countdown / 60;   // Calculate new mins
+            sec = countdown % 60;   // Calculate new secs
             DispTime(min, sec);
         }
 
+        // Once countdown reaches 0
         if (countdown == 0) {
-            alarm = 1;
-            running = 0;
-            T2CONbits.TON = 0; 
-            Disp2String("\rFIN 00m : 00s - ALARM");
+            alarm = 1;  // Activate alarm
+            running = 0;    // Stop the program from running
+            T2CONbits.TON = 0;  // Stop timer 2
+            Disp2String("\rFIN 00m : 00s - ALARM"); // Display alarm message
         }
     }
 }
 
+
+// Controls the LED flashing while alarm is active
 void alarm_flash(void){
     // Set LED1 ON and flash LED2 rapidly
     LATBbits.LATB9 = 1;
     if(T1flag){
-        T1flag = 0;
-        TMR1 = 0;
-        T1CONbits.TON = 1;
+        T1flag = 0;     //Reset flag
+        TMR1 = 0;       //Reset count for t1
+        T1CONbits.TON = 1;  // Restart T1
         LATAbits.LATA6 = !LATAbits.LATA6; //Flash LED2
     }
 }
